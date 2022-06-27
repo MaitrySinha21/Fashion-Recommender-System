d59b74c8c857e2880d2639ebf912296253bfb381
Maitry Sinha <85226862+MaitrySinha21@users.noreply.github.com>
2022-06-27 09:48:06 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,29 +1,41 @@
-from flask import Flask,render_template,request
+from flask import Flask, render_template, request
 from flask_cors import cross_origin
-from detection import mobilenet, Decode
+import cv2
+import pandas as pd
+from sklearn.neighbors import NearestNeighbors
+from utils import embeding_image, get_indices, recomendations, Features, Decode
+
+neighbors = NearestNeighbors(n_neighbors=6, algorithm='brute', metric='euclidean')
+
+# load info file
+df = pd.read_csv('data/info.csv')
+
+# load features of fashion images
+features = Features()
+
 
 app = Flask(__name__)
 
-@app.route('/',methods=['GET'])
+
+@app.route('/', methods=['GET'])
 @cross_origin()
 def index():
     return render_template('index.html')
 
-@app.route('/result',methods=['GET','POST'])
+
+@app.route('/result', methods=['GET', 'POST'])
 @cross_origin()
 def result():
     if request.method == 'POST':
         image = request.json['image']
         img = Decode(image).copy()
-        #img = cv2.resize(img, (480, 320),interpolation=cv2.INTER_NEAREST)
-        mobilenet(img)
+        cv2.imwrite('static/input.jpg', img)
+        input_emb = embeding_image(img)
+        indices = get_indices(input_emb, features, neighbors)
+        recomendations(df, indices)
         return render_template('index.html')
     return render_template('index.html')
 
 
-
-if __name__=="__main__":
-    app.run(host="0.0.0.0",port="5000")
-
-# Author: Swati Sinha & Maitry Sinha
-# Date : 2-09-2021
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
